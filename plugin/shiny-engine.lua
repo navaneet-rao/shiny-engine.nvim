efbf4f6810d71d3249721567abbeb05aa635eb19
Navaneet R Rao <navaneet517@gmail.com>
2024-04-11 19:12:34 +0530

    my first neovim plugin

---
+++ plugin/shiny-engine.lua
@@ -0,0 +1,4 @@
+-- print("plugin/shiny-engine.lua is exected!")
+if vim.fn.has("nvim-0.7.0") ~= 1 then
+  vim.api.nvim_err_writeln("navaneet-rao/shiny-engine.nvim requires at least nvim-0.7.0.")
+end
